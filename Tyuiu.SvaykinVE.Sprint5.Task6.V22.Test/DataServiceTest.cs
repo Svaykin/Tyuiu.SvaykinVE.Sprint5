b8abeaf5aba976b74db7647dc685fb352e13b36b
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-26 04:26:03 +0500

    Task6

---
+++ Tyuiu.SvaykinVE.Sprint5.Task6.V22.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.SvaykinVE.Sprint5.Task6.V22.Lib;
+using System.IO;
+namespace Tyuiu.SvaykinVE.Sprint5.Task6.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V22.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V22.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
